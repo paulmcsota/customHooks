10a438b86e49c9fd9900e70d0ad286a5eeab0f0d
Paul Mesco Sota <paulmcsota@gmail.com>
2020-11-10 11:17:42 -0500

    Add readme of hooks

--- useCounter/useCounter.js
+++ useCounter/useCounter.js
@@ -1,20 +1,20 @@
 import { useState }from 'react';
 
 export const useCounter = (initialState = 10) => {
-   const [state, setState] = useState(initialState);
+   const [counter, setState] = useState(initialState);
 
    const increment = () => {
-      setState(state + 1);
+      setState(counter + 1);
    };
    const decrement = () => {
-      setState(state - 1);
+      setState(counter - 1);
    };
    const reset = () => {
       setState(initialState);
    }
 
    return {
-      state,
+      counter,
       increment,
       decrement,
       reset
---
+++ useForm/README.md
@@ -0,0 +1,11 @@
+# useForm Hook
+
+Ejemplo de uso
+```
+   const initialForm = {
+      name: '',
+      age: 0,
+      email: '',
+   };
+   return [values, handleInputChange, reset] = useForm(initialForm);
+```
